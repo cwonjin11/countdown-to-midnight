fcb776e542945f1de4415863b97668447f8c6e5e
Wonjin <cwonjin1@gmail.com>
2020-12-09 10:28:43 -0500

    commit tree b006dbe7143ce9ecabab62d0f5e149e6991d7840

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,22 @@
 #write your code here
 
-def countdown
+def countdown(x)
+    x = 10
+    while x > 0
+        puts "#{x} SECOND(S)!"
+        x -= 1
+    end
+    return "HAPPY NEW YEAR!"
 end
+
+def countdown_with_sleep(seconds)
+    while seconds > 0
+      puts "#{seconds} SECOND(S)!"
+      seconds -= 1
+      sleep(1)
+    end
+    return "HAPPY NEW YEAR!"
+  end
+
+
+    
